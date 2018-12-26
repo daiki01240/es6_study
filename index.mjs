07a9887051e538c4c2bc635982a939a2b20c6da3
daiki01240 <domo01240@gmail.com>
2018-12-26 22:38:41 +0900

    [add] array

--- index.mjs
+++ index.mjs
@@ -1,78 +1,58 @@
-// //class 
-// var Person = function (name, age) {
-//     this.name = name;
-//     this.age = age;
-// };
- 
-// Person.prototype.jump = function () {
-//     console.log("ジャンプ");
-// };
+// //分割代入
+// var numbers = [1,2,3,4];
+// let [a,b,c,d] = numbers;
+// console.log(a)
 
-// var person = new Person("太郎", 30);
-// person;
-// person.jump;
+// function getConfig(){
+//     return[
+//         true,
+//         10,
+//         1,
+//         2,
+//         3
+//     ]
+// }
 
-class Person {
-    constructor(name,age){
-        this.name = name;
-        this.age = age;
-    }
-    getName(){
-        return this.name;
-    }
-    static setName(person, name){
-        person.name = name; 
-    }
-    // jump(){
-    //     console.log("jump");
-    // }
-}
-let person = new Person("太郎", 30);
-// person.jump();
-//console.log(person);
+// const config = getConfig();
+// console.log(config[0]);
 
-class Employee extends Person {
-  constructor(name, age, years) {
-    super(name, age);
-    this.years = years;
-    } 
-    getName() {
-        return super.getName() + "従業員";
-    }
-//   jump() {
-//       super.jump();
-//     console.log("jumpJUMP");
-//   }
-  quit() {
-    console.log(`I am quit this work ${this.years}`);
-  }
+// const [isOn, amount,...data] = getConfig();
+// console.log(isOn);
+// console.log(amount);
+// console.log(data);
+
+// let _isOn = false;
+// let _amount = 10;
+
+// function setConfig(config){
+//     _isOn = config[0];
+//     _amount = config[1];
+// }
+// function setConfig([isOn, amount]) {
+//     _isOn = isOn;
+//     _amount = amount;
+// }
+
+// setConfig([true,20]);
+
+var config = {};
+function setConfig({isOn,amount}){
+    config = {isOn,amount};
+    console.log(config);
 }
 
-let employee = new Employee("taro", 20,15);
-//employee.jump();
-//employee.quit();
-console.log(employee.getName());
+setConfig({ isOn: true, amount: 30});
 
-Person.setName(employee, "ziro");
-console.log(employee) ;
-
-class Student {
-    constructor(name,age){
-        this._name = name;
-        this._age = age;
-    }
-    get name(){
-        return "["+ this._name + "]";
-    }
-    set name(name){
-        this._name = name;
+function  getConfig() {
+    return {
+        isOn: true,
+        amount: 10,
+        servers:{
+            a:"abcd",
+            b:"efgh"
+        }
     }
 }
 
-let student = new Student("太郎",20);
-console.log(student);
-let name = student.name;
-console.log(name);
-
-student.name = "daigorou";
-console.log(student._name);
+let {isOn: onOffInfo, amount:dataAmount} = getConfig();
+console.log(onOffInfo, dataAmount);
