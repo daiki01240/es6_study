6adce3efbd61138fe0a985f90e5ea259d860b8b3
daiki01240 <domo01240@gmail.com>
2018-12-29 00:11:40 +0900

    [add] promise

--- index.mjs
+++ index.mjs
@@ -1,30 +1,83 @@
-// generator
-// let arr = [10,20,30];
+// //Promise
+// var func_one = function(callback,int){
+//     console.log("The First"+int);
+//     if(callback){
+//         callback();
+//     }
+// };
 
-// for (n of arr.entries()){
-//     console.log(n);
+// var func_two = function (callback) {
+//     console.log("The Two");
+//         if (callback) {
+//             callback();
+//         }
+// };
+
+// var func_three = function (callback) {
+//     console.log("The Three");
+//     if (callback) {
+//         callback();
+//     }
+// };
+
+// func_one(function(){
+//     func_two(function(){
+//         func_three();
+//     });
+// },1);
+
+// function getName(){
+//     setTimeout(()=>{
+//         console.log("dadada");
+//     },2000);
 // }
 
-// function* myGenerator(){
-//     console.log("a");
-//     yield 1;
-//     console.log("i");
-//     yield 2;
-//     console.log("u");
-//     yield 3;
-//     console.log("e");
-// }
+// getName();
 
-// let iter = myGenerator();
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
-let obj = {
-    *myGenerator(){
-        yield 1;
-    }
+function getName() {
+    return new Promise((resolve,reject) => {
+        throw new Error("Errorrr");;
+    });
 }
 
-let iter = obj.myGenerator();
-console.log(iter.next());
+function getAge(){
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(20);            
+        }, 4000);
+    });
+}
+
+// Promise.all([
+//     getName(),
+//     getAge()
+// ])
+// .then(([name,age]) =>{
+//     console.log(name);
+//     console.log(age);
+// });
+
+
+var fff =  getName()
+ .then(name => {
+     console.log(name);
+ })
+ .catch(err => {
+     console.log(err);
+ })
+ .then(value=>{
+     console.log(value);
+     console.log("Finalaaaaaaaaaaaa");
+ });
+//  .then(getAge)
+//  .then(age => {
+//      console.log(age);
+//  });
+
+function getNameAll() {
+    return Promise.resolve("taro")
+}
+
+getNameAll().then(name => {
+    console.log(name);
+})
